9a4c8e2cf971fc6d9a1818017b447b8a97bcb1d8
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2017-06-12 22:12:03 -0300

    Desenvolver formato dos segmentos a ser utilizado.

--- src/protocol/PReceiver.java
+++ src/protocol/PReceiver.java
@@ -9,48 +9,57 @@ import java.io.IOException;
 
 /*	PacketReceiver, implementa parte receptora de datagramas da Socket	*/
 
-public class PReceiver {
+public class PReceiver implements Runnable {
 	/*	time de timeout
 	 * 	DatagramSocket para receber datagramas
 	 * 	PipedOutputStream para enviar dados à aplicação */
-	private int time;
 	private DatagramSocket datagramSocket;
+	private DatagramPacket datagramPacket;
 	private PipedOutputStream dataOut;
+	private byte buf[];
 	
 	//boolean determinará se thread deve continuar rodando
 	public boolean alive;
 	
-	public PReceiver(int port, int time, PipedInputStream dataReader) {
-		this.time = time;
+	public PReceiver(int port, PipedInputStream dataReader, DatagramSocket datagramSocket) {
 		alive = true;
-		datagramSocket = null;
 		dataOut = null;
+		buf = new byte[1500];
+		datagramPacket = new DatagramPacket(buf, buf.length);
+		this.datagramSocket = datagramSocket;
 		try {
 			dataOut = new PipedOutputStream(dataReader);
-			datagramSocket = new DatagramSocket(port);
-			datagramSocket.setSoTimeout(time);
-		} catch (SocketException se) {
-
 		} catch (IOException ioe) {}
+
 	}
 	
-	private String decapsulateDatagram(DatagramPacket datagram) {
-		String message = null;
+	public void run() {
+		try {
+			datagramSocket.receive(datagramPacket);
+		} catch (IOException ioe) {}
+		
+		switch (judgeDatagram(datagramPacket)) {
+			// 0 - Datagrama OK, entregar mensagem à aplicação
+			case (0):
+				try {
+					dataOut.write(decapsulateDatagram(datagramPacket));
+				} catch (IOException ioe) {}
+			break;
+
+			default:
+			break;
+		}
+	}
+	
+	/*	Método para extrair mensagem de segmento */
+	private byte[] decapsulateDatagram(DatagramPacket datagram) {
+		byte message [] = null;
 		
 		return message;
 	}
 	
 	/*	Método para julgar datagrama e decidir como proceder */
-	
 	private int judgeDatagram(DatagramPacket toJudge) {
 		return 0;
-	}
-	
-	private void getMessage() {
-		DatagramPacket temp = null;
-		try {
-			datagramSocket.receive(temp);
-		} catch (IOException ioe) {}
-		
 	}
 }
